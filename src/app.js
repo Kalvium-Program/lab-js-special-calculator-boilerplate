0bea261414f2850810abf0cbd01e66e5a3e00b18
Kalvium-Program <109285740+Kalvium-Program@users.noreply.github.com>
2022-11-24 11:14:49 +0530

    modified readme

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 // Progression 1: Create special addition
 
+// function specialAddition(){}
+
 // Progression 2: Create simple division
 
 // Progression 3: Create the special calculator
-
-// creating a doc
-// test description to be more clear
-// readme file.
